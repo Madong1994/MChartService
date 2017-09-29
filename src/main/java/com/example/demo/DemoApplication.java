d0c3c260ea22596c0ca21d76ef9d1fbbc79fd8c3
md <123456>
2017-09-29 14:52:47 +0800

    完成登录handler

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -2,6 +2,8 @@ package com.example.demo;
 
 import com.jfinal.log.Log;
 import im.server.IMServerStarter;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
@@ -10,7 +12,7 @@ import org.springframework.context.annotation.ComponentScan;
 @SpringBootApplication
 @ServletComponentScan
 public class DemoApplication {
-	private static Log log = Log.getLog(DemoApplication.class);
+	private static Logger log = LoggerFactory.getLogger(DemoApplication.class);
 	public static void main(String[] args) {
 		boolean isStart = IMServerStarter.start();
 		if(isStart){
--- src/main/java/im/common/handlers/impl/LoginHandler.java
+++ src/main/java/im/common/handlers/impl/LoginHandler.java
@@ -4,47 +4,46 @@ package im.common.handlers.impl;
 import com.alibaba.fastjson.JSONObject;
 import com.example.demo.entity.User;
 import com.example.demo.service.UserService;
-import com.jfinal.log.Log;
 import im.common.IMPacket;
 import im.common.handlers.BaseHandler;
 import im.common.protof.RequestModel;
 import im.common.protof.ResponseModel;
+import im.common.util.RequestCode;
 import im.common.util.annotation.IMInterceptor;
 import im.common.util.annotation.IMRequest;
-import im.common.util.tool.IMSend;
-import im.common.util.tool.ProtoBufUtil;
-import im.common.util.tool.ResultMsg;
-import im.common.util.tool.ResultMsgCode;
+import im.common.util.tool.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.tio.core.Aio;
 import org.tio.core.ChannelContext;
 
+import java.io.UnsupportedEncodingException;
+import java.security.NoSuchAlgorithmException;
 
-/**
- * All rights Reserved, Designed By hxjd
- *
- * @类名: LoginHandler.class
- * @包名: com.base.im.common.handlers.impl
- * @描述: 登录
- * @所属: 华夏九鼎
- * @日期: 2017/8/16 14:34
- * @版本: V1.0
- * @创建人：马东
- * @修改人：马东
- * @版权: 2017 hxjd Inc. All rights reserved.
- * 注意：本内容仅限于华夏九鼎内部传阅，禁止外泄以及用于其他的商业目的
- */
-@IMRequest(requestCode = 0)
+
+
+@IMRequest(requestCode = RequestCode.LOGIN)
 @IMInterceptor
 public class LoginHandler implements BaseHandler {
     @Autowired
     UserService userService;
-    private static Log log = Log.getLog(LoginHandler.class);
+    private static Logger log = LoggerFactory.getLogger(LoginHandler.class);
     @Override
     public String init(RequestModel.ImRequest imRequest, ChannelContext<Object, IMPacket, Object> channelContext) {
         String userJson = imRequest.getData();
         User user = User.toUser(userJson);
-        User muser = userService.selectUserByUserNumAndPwd(user.getUserNum(),user.getPwd());
+        String pwd = "";
+        try {
+            pwd= MD5Util.EncoderByMD5(user.getPwd());
+        } catch (NoSuchAlgorithmException e) {
+            log.info(e.getMessage());
+            e.printStackTrace();
+        } catch (UnsupportedEncodingException e) {
+            log.info(e.getMessage());
+            e.printStackTrace();
+        }
+        User muser = userService.selectUserByUserNumAndPwd(user.getUserNum(),pwd);
         if(muser == null){
             //没有注册
             ResultMsg resultMsg = new ResultMsg();
